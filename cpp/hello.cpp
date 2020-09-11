1f0001ea370efd6bc14802b37142c77841577a11
GratisLPG <maciej.prus@lo1.sandomierz.pl>
2020-09-11 09:10:53 +0200

    dodanie plików źródłowych cpp i python

---
+++ cpp/hello.cpp
@@ -0,0 +1,18 @@
+/*
+ * hello.cpp
+ * Wyświetlenie tekstu w terminalu
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "Hello!";
+	return 0;
+}
+
